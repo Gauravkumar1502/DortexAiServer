83820b59b6941040fe7d4e889037ec86457c9daa
Gauravkumar1502 <gauravkumar15021999@gmail.com>
2024-08-30 20:42:22 +0530

    Email Server Done

---
+++ index.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to DortexAi API');
+});
+// send email
+app.post('/send-email', (req, res) => {
+  const { name, email, subject, message } = req.body;
+  console.warn(`name: ${name},\nemail: ${email},\nsubject: ${subject},\nmessage: ${message}`);
+  console.warn(`APP_EMAIL: ${process.env.APP_EMAIL},\nAPP_PASSWORD: ${process.env.APP_PASSWORD},\nTO_EMAIL: ${process.env.TO_EMAIL}`);
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.APP_EMAIL,
+            pass: process.env.APP_PASSWORD
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.APP_EMAIL,
+        to: process.env.TO_EMAIL,
+        subject: `${name} - ${subject}`,
+        html: `Dear DortexAi Team,<br>
+            <P>
+                ${message}
+            </P>
+            <strong>Please respond to the user as soon as possible.</strong><br><br>
+            Best Regards,<br>
+            ${name}<br>
+            ${email}<br>`
+    };
+    
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            return res.status(500).send(error.message);
+        } else {
+            console.log('Email sent: ' + info.response);
+            return res.status(200).send('Email sent successfully');
+        //   handleSuccess(res, info.response, "Email sent successfully", statusCode.OK);
+        }
+    });
+    res.send('Email sent successfully');
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
